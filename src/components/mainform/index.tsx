2b6d7c01760837feba2ae2d630fa8d5c73cec3b0
Hilder Gill <hildergill@gmail.com>
2022-09-14 08:46:49 +0800

    Added the props for the MainForm component

--- src/components/mainform/index.tsx
+++ src/components/mainform/index.tsx
@@ -1,8 +1,22 @@
-import { FC } from "react";
+import { FC, FormEvent, FormEventHandler } from "react";
 
-export const MainForm: FC = () => {
+export type OnSubmitMainForm = {
+	(files: File[]): void;
+};
+
+export type MainFormProps = {
+	onSubmit?: OnSubmitMainForm;
+};
+
+export const MainForm: FC<MainFormProps> = (props: MainFormProps) => {
+	const onSubmit: FormEventHandler = (event: FormEvent) => {
+		event.preventDefault();
+
+		/** @Todo Add something here later */
+	};
+
 	return (
-		<form>
+		<form onSubmit={onSubmit}>
 			<label htmlFor="fileInput">Upload your file</label>
 			<input type="file" name="fileInput" id="fileInput" accept="image/*" required />
 			<input type="reset" value="Reset" />
