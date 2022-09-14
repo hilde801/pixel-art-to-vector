a9bb072535a5e48ad57a830e3d02d2f7f7225c41
Hilder Gill <hildergill@gmail.com>
2022-09-14 15:29:38 +0800

    Added the dedicated handler function

--- src/components/appcomponent/index.tsx
+++ src/components/appcomponent/index.tsx
@@ -1,17 +1,21 @@
 import { FC, useState } from "react";
 import { FooterComponent } from "../footercomponent";
 import { HeaderComponent } from "../headercomponent";
-import { MainForm } from "../mainform";
+import { MainForm, OnSubmitMainForm } from "../mainform";
 
 export const AppComponent: FC = () => {
 	const [files, setFiles] = useState<File[]>([]);
+
+	const onSubmitMainForm: OnSubmitMainForm = (files: File[]) => {
+		/** @Todo Add something here later */
+	};
 
 	return (
 		<>
 			<HeaderComponent />
 
 			<main>
-				{files.length <= 0 && <MainForm onSubmit={(files: File[]) => setFiles(files)} />}
+				{files.length <= 0 && <MainForm onSubmit={onSubmitMainForm} />}
 				{files.length > 0 && <p>Process files here...</p>}
 			</main>
 
