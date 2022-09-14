fed974fee802f454c9fe9b381e46f54cf28e4cde
Hilder Gill <hildergill@gmail.com>
2022-09-14 14:33:55 +0800

    Added the Pixel class

---
+++ src/lib/pixel.ts
@@ -0,0 +1,32 @@
+import { createElement, DetailedHTMLProps, HTMLProps, ReactSVGElement, SVGAttributes } from "react";
+import Color from "./color";
+
+class Pixel {
+	private _x: number;
+	private _y: number;
+	private _color: Color;
+
+	constructor(x: number, y: number, color: Color) {
+		this._x = x;
+		this._y = y;
+		this._color = color;
+	}
+
+	public getElement(key: number): ReactSVGElement {
+		const props: SVGAttributes<SVGRectElement> = {
+			x: this._x,
+			y: this._y,
+			width: 1,
+			height: 1,
+			style: {
+				fill: this._color.hexString,
+				fillOpacity: this._color.alpha / 255
+			}
+		};
+
+		(props as any).key = key;
+		return createElement("rect", props);
+	}
+}
+
+export default Pixel;
