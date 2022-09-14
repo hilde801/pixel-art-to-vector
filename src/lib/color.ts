af66a5621cec41b145acbc3f6099520595351b36
Hilder Gill <hildergill@gmail.com>
2022-09-14 14:15:55 +0800

    Added the color class

---
+++ src/lib/color.ts
@@ -0,0 +1,45 @@
+class Color {
+	private _red: number;
+	private _green: number;
+	private _blue: number;
+	private _alpha: number;
+
+	constructor(red: number, green: number, blue: number, alpha: number) {
+		this._red = red;
+		this._green = green;
+		this._blue = blue;
+		this._alpha = alpha;
+	}
+
+	private static generateColorHex = (color: number): string => {
+		const hex: string = color.toString(16).toLocaleUpperCase();
+		return hex.length == 2 ? hex : `0${hex}`;
+	};
+
+	public get hexString(): string {
+		const redString: string = Color.generateColorHex(this._red),
+			greenString: string = Color.generateColorHex(this._green),
+			blueString: string = Color.generateColorHex(this._blue),
+			alphaString: string = Color.generateColorHex(this._alpha);
+
+		return `#${redString}${greenString}${blueString}${alphaString}`;
+	}
+
+	public get red(): number {
+		return this._red;
+	}
+
+	public get green(): number {
+		return this._green;
+	}
+
+	public get blue(): number {
+		return this._blue;
+	}
+
+	public get alpha(): number {
+		return this._alpha;
+	}
+}
+
+export default Color;
