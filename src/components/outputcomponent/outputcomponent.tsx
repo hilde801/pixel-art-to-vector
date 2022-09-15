4f4e5f9a42d4a0fe468112590a0bc2bb75d6fa58
Hilder Gill <hildergill@gmail.com>
2022-09-15 14:04:26 +0800

    This is good

--- src/components/outputcomponent/outputcomponent.tsx
+++ src/components/outputcomponent/outputcomponent.tsx
@@ -1,23 +1,38 @@
 import JSZip from "jszip";
-import { FC, ReactElement, useContext } from "react";
+import { FC, ReactElement, useContext, useState } from "react";
+import { Download } from "react-feather";
 import { TasksContext } from "../../contexts/taskscontext";
 import Task from "../../lib/task";
+import { IconAnchor } from "../iconanchor";
 import { TasksListItem } from "./taskslistitem";
 
 export const OutputComponent: FC = () => {
+	const [downloadLink, setDownloadLink] = useState<string>();
+
 	const { tasks } = useContext(TasksContext);
 
-	const tasksListItems: ReactElement[] = [];
+	const jsZip: JSZip = new JSZip(),
+		tasksListItems: ReactElement[] = [];
 
 	tasks.forEach((task: Task, index: number) => {
+		if (task.elementString) {
+			jsZip.file(`${task.originalFilename}.svg`, task.elementString);
+		}
+
 		tasksListItems.push(<TasksListItem {...task} key={index} />);
 	});
+
+	jsZip.generateAsync({ type: "base64" }).then((base64: string) => {
+		const tempDataUrl: string = `data:application/zip;base64,${base64}`;
+		setDownloadLink(tempDataUrl);
+	});
+
+	const downloadFilename: string = btoa(Date.now().toString()).replaceAll("=", "");
 
 	return (
 		<output>
 			<ul>{tasksListItems}</ul>
-
-			{/** @Todo Add something here later */}
+			{downloadLink && <IconAnchor text="Download" icon={<Download />} download={downloadFilename} href={downloadLink} />}
 		</output>
 	);
 };
