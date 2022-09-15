006ad4df205b9792be22e5e6d15eb7d9f18ae54c
Hilder Gill <hildergill@gmail.com>
2022-09-15 14:16:02 +0800

    Moved localizations files to its own directory

--- src/localizations/i18next.ts
+++ src/localizations/i18next.ts
@@ -2,9 +2,9 @@ import i18next from "i18next";
 import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-import supportedLngs from "../../locales/languages.json";
+import supportedLngs from "./languages.json";
 
-import en from "../../locales/en.json";
+import en from "./en.json";
 
 i18next.use(initReactI18next).use(I18nextBrowserLanguageDetector).init({
 	supportedLngs,
